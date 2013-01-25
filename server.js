49da717fe95faca530dbb776b3ad91e4167e72c2
Jahfer Husain <jahferh@gmail.com>
2013-01-25 18:53:21 -0500

    Added socket.io library

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var express = require('express');
+var express = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
 
-var app = express.createServer(express.logger());
+server.listen(80);
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + "/index.html");
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
+
+/*var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	response.send('Hello world!');
@@ -9,5 +24,5 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log('Listening on ' + port);
-});
+});*/
 
