e73cfa1cbce5e9dc0320d9333106f859c9f13eb9
Jahfer Husain <jahferh@gmail.com>
2013-01-27 00:33:24 -0500

    Removed pipes :(

--- public/js/Bonestorm_test.js
+++ public/js/Bonestorm_test.js
@@ -181,7 +181,7 @@ var Bonestorm = (function (_super) {
         for(var i in this.opponentBullets) {
             this.opponentBullets[i].draw();
         }
-        this.pipes.draw();
+        //this.pipes.draw();
     };
     Bonestorm.prototype.exit = function () {
     };
